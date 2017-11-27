d8ed67f396b58d0256dec523129214e69810a4ad
nick.windham <nick.windham@angryhangar.com>
2017-11-28 08:39:55 +0900

    Almost-working credits. Needs loading improvements.

--- src/MazeGUI.java
+++ src/MazeGUI.java
@@ -1,3 +1,4 @@
+import javafx.animation.*;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -19,9 +20,14 @@ import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import java.io.File;
 import java.io.FileInputStream;
+import java.time.Duration;
 
 public class MazeGUI extends Application
 {
+
+    //variable to control timeline
+    private int millis = 0;
+
     @Override // Override the start method in the Application class
     public void start(Stage primaryStage) throws Exception
     {
@@ -93,22 +99,80 @@ public class MazeGUI extends Application
         triWizardCup.setFitWidth(100);
         finalPane.getChildren().addAll(finalText,triWizardCup);
 
+
         //set credits. Whoo!
         Text txtProjectBy = new Text("Project By...");
         txtProjectBy.setFont(Font.font("Ariel",50));
+        //this puts the text waaaaay off in the distance, so it's not on screen until it needs to be.
+        txtProjectBy.xProperty().bind(border.widthProperty().add(3000));
+
         Text txtCam = new Text("Cam Collins");
         txtCam.setFont(Font.font("Ariel", 40));
+        txtCam.xProperty().bind(border.widthProperty().add(3000));
+
         Text txtCruise = new Text("Cruise Gatzman");
         txtCruise.setFont(Font.font("Ariel", 40));
+        txtCruise.xProperty().bind(border.widthProperty().add(3000));
+
         Text txtNick = new Text("Nick Windham");
         txtNick.setFont(Font.font("Ariel", 40));
+        txtNick.xProperty().bind(border.widthProperty().add(3000));
+
+        Text txtHim = new Text("\"He\" has come again...");
+        txtHim.setFont(ftHPFONT);
+        txtHim.xProperty().bind(border.widthProperty().add(3000));
+
+        //credits pane
+        BorderPane creditsPane = new BorderPane();
+        creditsPane.setBackground(new Background(controlBackground));
+
+
         //invisible line to roll credits
         Line scrollLine = new Line();
         scrollLine.setStroke(Color.TRANSPARENT);
-        //credits pane
-        BorderPane creditsPane = new BorderPane();
+        //bind line to the center of screen going down
+        scrollLine.startXProperty().bind(creditsPane.widthProperty().divide(2));
+        scrollLine.endXProperty().bind(creditsPane.widthProperty().divide(2));
+        scrollLine.startYProperty().bind(creditsPane.heightProperty().add(400));
+        scrollLine.endYProperty().bind(creditsPane.heightProperty().subtract(3000));
+        creditsPane.getChildren().add(scrollLine);
 
+        //"his" line to scroll. stops midway to display on screen.
+        Line scrollHisLine = new Line();
+        scrollHisLine.setStroke(Color.TRANSPARENT);
+        //bind line to the center of screen going down
+        scrollHisLine.startXProperty().bind(creditsPane.widthProperty().divide(2));
+        scrollHisLine.endXProperty().bind(creditsPane.widthProperty().divide(2));
+        scrollHisLine.startYProperty().bind(creditsPane.heightProperty().add(400));
+        scrollHisLine.endYProperty().bind(creditsPane.heightProperty().divide(2));
+        creditsPane.getChildren().add(scrollHisLine);
 
+        //set path transitions for text
+        PathTransition pathPresentedBy = new PathTransition();
+        PathTransition pathCam = new PathTransition();
+        PathTransition pathCruise = new PathTransition();
+        PathTransition pathNick = new PathTransition();
+        PathTransition pathHim = new PathTransition();
+        //set node
+        pathPresentedBy.setNode(txtProjectBy);
+        pathCam.setNode(txtCam);
+        pathCruise.setNode(txtCruise);
+        pathNick.setNode(txtNick);
+        pathHim.setNode(txtHim);
+        //set path
+        pathPresentedBy.setPath(scrollLine);
+        pathCam.setPath(scrollLine);
+        pathCruise.setPath(scrollLine);
+        pathNick.setPath(scrollLine);
+        pathHim.setPath(scrollHisLine);
+        //set rate
+        pathPresentedBy.setRate(.01);
+        pathCam.setRate(.01);
+        pathCruise.setRate(.01);
+        pathNick.setRate(.01);
+        pathHim.setRate(.1);
+        //add all the text
+        creditsPane.getChildren().addAll(txtProjectBy,txtCam,txtCruise,txtNick,txtHim);
 
 
         ImageView harryPotterMouseStart = new ImageView(new Image("images/harrypotter.png"));
@@ -132,6 +196,11 @@ public class MazeGUI extends Application
         Media finishedSound = new Media(new File("src/sounds/FinishCheer.wav").toURI().toString());
         MediaPlayer finishedPlayer = new MediaPlayer(finishedSound);
 
+        //Credits music
+        Media creditTheme = new Media(new File("src/sounds/somber.mp3").toURI().toString());
+        MediaPlayer creditPlayer = new MediaPlayer(creditTheme);
+
+        //Movie clip
         Media harryPotterClip = new Media(new File("src/video/HarryPotterFinalClip.mp4").toURI().toString());
         MediaPlayer hpClipPlayer = new MediaPlayer(harryPotterClip);
         MediaView hpClipViewer = new MediaView(hpClipPlayer);
@@ -344,6 +413,71 @@ public class MazeGUI extends Application
             finishedPlayer.play();
         });
 
+        //set timeline to handle animation and timing for video-to-credits transition
+        AnimationTimer animTimerCredits = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                //increment time in ms
+                millis++;
+
+                //once 33seconds, do transition
+                if (millis == 3000){
+                    //kill audio feed from video
+                    hpClipPlayer.stop();
+
+                    //play audio for credit roll
+                    creditPlayer.play();
+
+                    //timeline will control actual animation, using Interpolator and translateXProperty
+                    Timeline timeline = new Timeline();
+                    KeyValue kv = new KeyValue(hpClipViewer.translateXProperty(), 0, Interpolator.EASE_BOTH);
+                    KeyFrame kf = new KeyFrame(javafx.util.Duration.seconds(1), kv);
+                    timeline.getKeyFrames().add(kf);
+                    //finish animation, clear previous pane and add in the credits pane
+                    timeline.setOnFinished(t->{
+                        border.getChildren().clear();
+                        border.setCenter(creditsPane);
+                    });
+                    timeline.play();
+                }
+
+                //check for 1 second after, animate credit roll
+                if (millis >= 3300){
+                    pathPresentedBy.play();
+                }
+                if (millis >= 3400){
+                    pathCam.play();
+                }
+                if (millis >= 3550){
+                    pathCruise.play();
+                }
+                if (millis >= 3600){
+                    pathNick.play();
+                }
+                if(millis >= 3900){
+                    pathHim.play();
+                }
+                if (millis >= 4100){
+                    pathHim.stop();
+                }
+
+                //set volume to fade, exit stage
+                if (millis == 5000){
+                    for (int i = 100; i >= 0; i--){
+                        creditPlayer.setVolume(i/100);
+                        if (i == 0){
+                            primaryStage.close();
+                        }
+                    }
+
+                }
+            }
+        };
+
+        //stop text after anim finishes
+        pathHim.setOnFinished(event -> pathHim.stop());
+
+
         finalPane.setOnMouseClicked(event ->
         {
             bgPlayer.stop();
@@ -351,8 +485,12 @@ public class MazeGUI extends Application
             border.setCenter(finalPane);
             finalPane.getChildren().clear();
             finalPane.getChildren().add(hpClipViewer);
+            //starts animation timer to auto-end segment
+            animTimerCredits.start();
             hpClipPlayer.play();
+
         });
+
 
         Scene scene = new Scene(border, 850, 600);
         primaryStage.setTitle("MazeRunner");
