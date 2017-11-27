668570267371d9cb36502d33aba09cb13923d561
CruiseGatzman <cgatzman@att.net>
2017-11-27 11:57:04 -0500

    Cleaned SFX, cleaned code, set BGM to repeat.

--- src/MazeGUI.java
+++ src/MazeGUI.java
@@ -6,7 +6,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
-import javafx.scene.media.MediaView;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
